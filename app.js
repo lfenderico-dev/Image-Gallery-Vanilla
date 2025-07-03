46c9a1099db0030e40b98c7b41d1069a7604db03
lfenderico-dev <lorenzofenderico.dev@gmail.com>
2025-07-03 11:20:20 +0200

    added the logic to scroll images with the mouse-wheel and with the buttons

---
+++ app.js
@@ -0,0 +1,21 @@
+// Add event listener to scroll with the mouse-wheel
+const container = document.getElementById('container');
+container.addEventListener(`wheel`, (event) => {
+    // prevent the default vertical scroll
+    event.preventDefault();
+    // scroll left the container based on how much you scrolled.
+    container.scrollLeft += event.deltaY;
+})
+
+// Add event listener to go back with the left button
+const leftBtn = document.getElementById('leftBtn');
+leftBtn.addEventListener('click', ()=>{
+    container.smoothScroll;
+    container.scrollLeft -= 900;
+})
+
+// Add event listener to go right with the right button
+const rgihtBtn = document.getElementById('rightBtn');
+rightBtn.addEventListener('click', ()=>{
+    container.scrollLeft += 900;
+})
